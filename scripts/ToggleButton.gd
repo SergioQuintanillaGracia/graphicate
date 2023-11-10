d7407eadec41d318fde6e89b87279d3f0c340cdd
SergioQuintanillaGracia <sergioquintanillagracia@gmail.com>
2023-11-10 15:13:29 +0100

    Added everything done to date

---
+++ scripts/ToggleButton.gd
@@ -0,0 +1,15 @@
+extends Control
+
+var anim_player
+
+func _ready():
+	anim_player = $"../../../../../AnimationPlayer"
+
+func _on_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.get_button_index() == 1 && event.is_pressed():
+			if $"../../..".custom_minimum_size[0] <= 120:
+				anim_player.play("SidebarAnimation")
+				
+			else:
+				anim_player.play_backwards("SidebarAnimation")
