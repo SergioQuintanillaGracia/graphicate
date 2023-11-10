2d949384d2c29a277443ab4fb5a279fa851c17ab
SergioQuintanillaGracia <sergioquintanillagracia@gmail.com>
2023-11-10 15:51:07 +0100

    Buttons, icons, current_mode

---
+++ scripts/main.gd
@@ -0,0 +1,45 @@
+extends Control
+
+
+const MODE_DRAW_VERTICES: int = 0
+const MODE_DRAW_EDGES: int = 1
+const MODE_EDIT: int = 2
+const MODE_DELETE: int = 3
+
+var current_mode: int
+
+
+func _ready():
+	current_mode = MODE_DRAW_VERTICES
+
+
+func _process(_delta):
+	pass
+
+
+func _on_draw_vertex_button_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.get_button_index() == 1 && event.is_pressed():
+			current_mode = MODE_DRAW_VERTICES
+			print("Changed mode to " + str(current_mode))
+
+
+func _on_draw_edge_button_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.get_button_index() == 1 && event.is_pressed():
+			current_mode = MODE_DRAW_EDGES
+			print("Changed mode to " + str(current_mode))
+
+
+func _on_edit_button_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.get_button_index() == 1 && event.is_pressed():
+			current_mode = MODE_EDIT
+			print("Changed mode to " + str(current_mode))
+
+
+func _on_delete_button_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.get_button_index() == 1 && event.is_pressed():
+			current_mode = MODE_DELETE
+			print("Changed mode to " + str(current_mode))
